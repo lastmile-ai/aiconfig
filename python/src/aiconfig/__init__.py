3ae63ca7e41c69c70ff6474516664efb661c0aab
Ankush Pala  ankush@lastmileai.dev <>
2023-10-23 09:17:06 -0400

    [Python] Ease of Use Changes
    - Modified imports in __init__.py
    - Updated import in test
    
    
    This diff Allows imports to be smaller and easier to use.
    
    `from aiconfig.AIConfigSettings import AIConfig` -> `from aiconfig import AIConfig`

---
+++ python/src/aiconfig/__init__.py
@@ -0,0 +1,13 @@
+from .AIConfigSettings import (
+    AIConfig,
+    ConfigMetadata,
+    ExecuteResult,
+    ModelMetadata,
+    Output,
+    Prompt,
+    PromptInput,
+    PromptMetadata,
+    SchemaVersion,
+)
+from .Config import AIConfigRuntime
+from .model_parser import ModelParser
