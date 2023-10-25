f12083459e43dfda8cf7a8f46c9ad0ee0c0f6095
Ankush Pala  ankush@lastmileai.dev <>
2023-10-25 17:59:02 -0400

    [Python] 4/n Serialize OpenAI Model Parser
    Modified serialize return type to list of Prompts.
    
    Implemented serialize for OpenAI ModelParser.
    
    Tested with api wrapper with the diff on top.

--- python/src/aiconfig/Config.py
+++ python/src/aiconfig/Config.py
@@ -127,9 +127,11 @@ class AIConfigRuntime(AIConfig):
         """
         model_parser = ModelParserRegistry.get_model_parser(model_name)
         if not model_parser:
-            raise ValueError(f"Unable to serialize data: `{data}`\n Model Parser for model {model_name} does not exist.")
-        
-        prompts = model_parser.serialize("", data, self, params)
+            raise ValueError(
+                f"Unable to serialize data: `{data}`\n Model Parser for model {model_name} does not exist."
+            )
+
+        prompts = model_parser.serialize(prompt_name, data, self, params)
         return prompts
 
     async def resolve(
