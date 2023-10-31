8a95fbbbcb82ba831f79e60bc3b15a1c8cafc45a
Ankush Pala  ankush@lastmileai.dev <>
2023-10-31 12:00:46 -0400

    [Python] Tests File paths absolute -> relative
    Updated tests to allow relative file path handling for opening aiconfigs while testing.
    
    - Created a util method that takes in a relative file path and converts it into an absolute file path
    - Updated tests to use Util method

---
+++ python/tests/util/file_path_utils.py
@@ -0,0 +1,16 @@
+import os
+
+def get_absolute_file_path_from_relative(working_file_path: str, relative_file_path: str) -> str:
+    """
+    Returns the absolute file path of a file given its relative file path and the file path of the calling file.
+    
+    Args:
+        working_file_path (str): The file path of the calling file.
+        relative_file_path (str): The relative file path of the file.
+    
+    Returns:
+        str: The absolute file path of the target file.
+    """
+    current_file_path = os.path.dirname(os.path.abspath(working_file_path))
+    absolute_file_path = os.path.join(current_file_path, relative_file_path)
+    return absolute_file_path
