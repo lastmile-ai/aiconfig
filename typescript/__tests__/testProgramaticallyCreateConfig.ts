92f6a4edfb97c3eb21672b31f9dd53d05f533781
Ankush Pala  ankush@lastmileai.dev <>
2023-11-03 11:35:06 -0400

    [Typescript] Introduce Extract Override Settings method
    This diff introduces the `extractOverrideSettings` function. One of the abstractions which will make it easier to construct model sttings
    
    - Abstracted existing logic to get overrides
    - Added test. Same test & logic as the python side

--- typescript/__tests__/testProgramaticallyCreateConfig.ts
+++ typescript/__tests__/testProgramaticallyCreateConfig.ts
@@ -1,5 +1,7 @@
 import { AIConfigRuntime } from "../lib/config";
-import { InferenceSettings } from "../types";
+import { JSONObject } from "../common";
+import { extractOverrideSettings } from "../lib/util/configUtils";
+import { InferenceSettings, ModelMetadata } from "../types";
 
 describe("test extract_override_settings", () => {
   // shared state
@@ -16,5 +18,71 @@ describe("test extract_override_settings", () => {
     const globalSettingsForTestModel =
       aiConfigRuntime.getGlobalSettings(modelId);
     expect(globalSettingsForTestModel).toEqual({ topP: 0.9 });
+  });
+});
+
+describe("test extract_override_settings", () => {
+  // shared state
+  let aiConfigRuntime: AIConfigRuntime =
+    AIConfigRuntime.create("Untitled AIConfig");
+
+  it("Test Case 1: No global setting, Expect an override", () => {
+    const initialSettings: InferenceSettings = { topP: 0.9 };
+    const modelId: string = "testmodel";
+
+    const override = extractOverrideSettings(
+      aiConfigRuntime,
+      initialSettings,
+      modelId
+    );
+    expect(override).toEqual(initialSettings);
+  });
+
+  it("Test Case 2: Global Settings differ, expect override", () => {
+    const initialSettings: InferenceSettings = { topP: 0.9 };
+    const modelId: string = "testmodel";
+
+    // Add a global setting that differs
+    aiConfigRuntime.addModel({ name: modelId, settings: { topP: 0.8 } });
+
+    const override = extractOverrideSettings(
+      aiConfigRuntime,
+      initialSettings,
+      modelId
+    );
+    expect(override).toEqual(initialSettings);
+  });
+
+  it("Test Case 3: Global Settings match settings, expect no override", () => {
+    const initialSettings: InferenceSettings = { topP: 0.9 };
+    const modelId: string = "testmodel";
+
+    // Update the global setting to match initialSettings
+    aiConfigRuntime.updateModel({
+      name: modelId,
+      settings: { topP: 0.9 },
+    } as ModelMetadata);
+
+    const override = extractOverrideSettings(
+      aiConfigRuntime,
+      initialSettings,
+      modelId
+    );
+    console.log("overidess: ", override);
+
+    expect(override).toEqual({});
+  });
+
+  it("Test Case 4: Global settings defined and empty settings defined, Expect no override", () => {
+    const modelId: string = "testmodel";
+
+    const inferenceSettings = {} as JSONObject;
+
+    const override = extractOverrideSettings(
+      aiConfigRuntime,
+      inferenceSettings,
+      modelId
+    );
+    expect(override).toEqual({});
   });
 });
