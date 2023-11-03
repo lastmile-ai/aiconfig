68dafe87c939d6a2123f1f2000cdeacf1ac6a885
Ankush Pala  ankush@lastmileai.dev <>
2023-11-03 14:16:37 -0400

    [Cookbook][Python] Added HuggingFace Text Generation Parser.
    HF text gen model parser.
    
    - Doesn't require an api key but it can be specified.
    - On startup, aiconfig will create a parser for Mistral.
    - Any others will need to be instantiated by the user.
    
    ```
    HF_parser = HuggingFaceTextParser("mistralai/Mistral-7B-Instruct-v0.1")
    
    config.register_model_parser(HF_parser)
    ```

---
+++ cookbook/HuggingFace/guide.py
@@ -0,0 +1,64 @@
+"""
+Integrating AIConfig with HuggingFace.
+
+This demo integrates AIConfig with HuggingFace. It shows how to use the HuggingFace model parser to run the `mistralai/Mistral-7B-Instruct-v0.1` model in AIConfig.
+"""
+import asyncio
+
+# AIConfig Imports
+from aiconfig import ModelParserRegistry
+from aiconfig import AIConfigRuntime
+from aiconfig.model_parser import InferenceOptions
+
+# Import the HuggingFace model parser
+from hf import HuggingFaceTextParser
+
+# Register ModelParser
+model_parser = HuggingFaceTextParser()
+ModelParserRegistry.register_model_parser(model_parser)
+
+# AIConfig under ."Mistral-aiconfig.json"
+# {
+#     "name": "exploring nyc through chatgpt config",
+#     "description": "",
+#     "schema_version": "latest",
+#     "metadata": {
+#         "parameters": {},
+#         "models": {
+#             "mistralai/Mistral-7B-v0.1": {
+#                 "model": "mistralai/Mistral-7B-v0.1",
+#                 "top_p": 0.9,
+#                 "temperature": 0.9,
+#                 "stream": True,
+#             }
+#         },
+#         "default_model": "mistralai/Mistral-7B-v0.1",
+#         "model_parsers": {
+#             "mistralai/Mistral-7B-v0.1": "HuggingFaceTextParser",
+#         },
+#     },
+#     "prompts": [
+#         {
+#             "name": "prompt1",
+#             "input": "Hi! Tell me 10 cool things to do in NYC.",
+#         }
+#     ],
+# }
+
+
+async def main():
+    config_file_path = "Mistral-aiconfig.json"
+    config_runtime = AIConfigRuntime.from_config(config_file_path)
+
+    # Define Streaming Callback Handler
+    def stream_to_stdout(data, accumulated, index):
+        print(data, end="")
+    inference_options = InferenceOptions(stream_callback=stream_to_stdout)
+
+    # Run the AIConfig
+    parameters = {}
+    await config_runtime.run("prompt1", parameters, inference_options)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
