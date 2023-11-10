290f070d77cb1681bd065977c176e2169e9c1206
Sarmad Qadri <sarmad@lastmileai.dev>
2023-11-09 23:52:06 -0500

    [Docs] Switch to SASS
    Change CSS to SCSS to fix some errors

--- aiconfig-docs/docusaurus.config.js
+++ aiconfig-docs/docusaurus.config.js
@@ -46,12 +46,12 @@ const config = {
           editUrl: "https://github.com/lastmile-ai/aiconfig/aiconfig-docs",
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve("./src/css/custom.scss"),
         },
       }),
     ],
   ],
-
+  plugins: ["docusaurus-plugin-sass"],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
