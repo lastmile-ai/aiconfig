e965566d45242b442c852508faf062f40e2b6f86
Sarmad Qadri <sarmad@lastmileai.dev>
2023-11-10 00:41:22 -0500

    [Docs] Add Algolia text search
    See https://docusaurus.io/docs/search#using-algolia-docsearch for more info

--- aiconfig-docs/docusaurus.config.js
+++ aiconfig-docs/docusaurus.config.js
@@ -169,6 +169,31 @@ const config = {
           "flow",
         ],
       },
+      algolia: {
+        appId: "MXLMDZIH1G",
+        apiKey: "ed56ae054b0f8ac02ef281e34584cc3c",
+        indexName: "aiconfig",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        //externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
     }),
 };
 
