849fb7b998c88d41360f21bb75925cbf09ca9fd6
Ankush Pala  ankush@lastmileai.dev <>
2023-11-15 16:06:21 -0500

    [cookbook] 3\n Remove HF extension hardcoded
    - remove modelparser from cookbook
    - refactored to import from extension
    - update config & code to instantiate model parser, use extension rather than the hard coded one

--- cookbooks/HuggingFace/typescript/ask-mistral.ts
+++ cookbooks/HuggingFace/typescript/ask-mistral.ts
@@ -7,20 +7,19 @@ import {
   AIConfigRuntime,
   InferenceOptions,
   InferenceCallbackHandlers,
+  ModelParserRegistry,
+
 } from "aiconfig";
 
-import { HuggingFaceTextGenerationModelParser } from "./hf";
+import { HuggingFaceTextGenerationModelParser } from "../../../extensions/HuggingFace/typescript/lib/hf";
 
 async function main() {
   const textGenerationModelParser = new HuggingFaceTextGenerationModelParser();
 
-  // reguster type error updated in unpublished version
-  // @ts-ignore: Unreachable code error
-  AIConfigRuntime.registerModelParser(textGenerationModelParser, [
-    "HuggingFaceTextParser",
-  ]);
-
   const config = AIConfigRuntime.load("../Mistral-aiconfig.json");
+  
+  ModelParserRegistry.registerModelParser( new HuggingFaceTextGenerationModelParser());
+
 
   function consoleLogCallback(
     data: string,
