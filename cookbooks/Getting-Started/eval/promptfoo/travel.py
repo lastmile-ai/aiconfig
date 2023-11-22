69ef8fa6936f0b2855f6997c1745252b4ca369d4
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-22 12:11:36 -0500

    [AIC][RFC] promptfoo integration prototype
    Make a custom provider using AIConfig and configure it with promptfoo yaml.

---
+++ cookbooks/Getting-Started/eval/promptfoo/travel.py
@@ -0,0 +1,31 @@
+import asyncio
+import os
+
+import openai
+from dotenv import load_dotenv
+
+from aiconfig import AIConfigRuntime
+from aiconfig.model_parser import InferenceOptions
+import sys
+
+
+async def main():
+    question = sys.argv[1]
+    aiconfig_path = "../../travel.aiconfig.json"
+
+    load_dotenv()
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    runtime = AIConfigRuntime.load(aiconfig_path)
+    inference_options = InferenceOptions(stream=False)
+
+    params = {
+        "the_query": question,
+    }
+
+    _ = await runtime.run("get_activities_parametrized", params, inference_options)
+    final_output = runtime.get_output_text("get_activities")
+    # print(final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
