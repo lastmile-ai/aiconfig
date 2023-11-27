19d8bf96ca26a76d247370e4752bc44aca386be2
Suyog Sonwalkar <suyog@lastmileai.dev>
2023-11-27 13:25:42 -0600

    Updating build script for CLI
    This gets a JS version of the CLI and editor working (so no typescript dependencies, etc.).
    
    Work in progress: Bundling dist with aiconfig npm repo and placing script into aiconfig/.bin folder to use as proper cli on publish to npm.

--- cli/index.ts
+++ cli/index.ts
@@ -1,10 +1,3 @@
-// Use commander or whatever to start CLI
-// Make it use dev mode for devserver, make it use built next app for regular usage
-// aiconfig devserver
-// aiconfig server
-
-// Should be only two commands for now, then most of the work is going to be in the next app & packaging it up properly as a correct CLI / putting stuff into the regular npm package as well
-
 import { program } from "commander";
 import { spawn } from "child_process";
 import path from "path";
@@ -17,13 +10,18 @@ program
 program
   .command("devserver")
   .option("--directory <directory>")
-  .option("--port <port>")
+  .option("-p, --port <port>", "Server port", "3000")
   .option("--plugins <directory>")
-  .action((str, options) => {
+  .action((options) => {
+    let port = 3000;
+    if (options.port) {
+      port = parseInt(options.port, 10);
+    }
+
     if (options.directory) {
       // TODO: Get the right directory to run from - aiconfig server itself needs access to the directory too
     } else {
-      spawn("yarn", ["dev"], {
+      spawn("yarn", ["dev", "-p", `${port}`], {
         cwd: path.join(process.cwd(), "./aiconfig-editor"),
         stdio: "inherit",
       });
@@ -32,12 +30,23 @@ program
 
 program
   .command("server")
-  .option("--port <port>")
+  .option("-p, --port <port>", "Server port", "3000")
   .option("--plugins <directory>")
-  .action((str, options) => {
+  .action((options) => {
+    let port = 3000;
+    if (options.port) {
+      port = parseInt(options.port, 10);
+    }
+
     // Use npm run start to start production server - this should work if they have to install the package from npm, if not, then will need to package node & stuff too
+    if (options.directory) {
+      // TODO: Get the right directory to run from - aiconfig server itself needs access to the directory too
+    } else {
+      spawn("yarn", ["start", "-p", `${port}`], {
+        cwd: path.join(process.cwd(), "./aiconfig-editor"),
+        stdio: "inherit",
+      });
+    }
   });
 
 program.parse();
-
-// const options = program.opts();
