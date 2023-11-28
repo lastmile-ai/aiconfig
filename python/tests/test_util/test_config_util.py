71b1df0d769eb0e76f19e33ac5d4c8c30ee61371
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-11-28 13:47:01 -0500

    [ez] linter autofix

--- python/tests/test_util/test_config_util.py
+++ python/tests/test_util/test_config_util.py
@@ -5,7 +5,7 @@ def test_get_api_key_from_environment():
     key = "TEST_API_KEY"
     try:
         get_api_key_from_environment(key)
-    except Exception as e:
+    except Exception:
         pass  # The expected exception was raised, so do nothing
     else:
         raise AssertionError("ExpectedException was not raised")
