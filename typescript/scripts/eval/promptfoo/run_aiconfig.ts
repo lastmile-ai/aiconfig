4a53c33a9da39758bb6bdafa1e5ca2ba3f3a815e
Jonathan Lessinger <jonathan@lastmileai.dev>
2023-12-04 12:30:47 -0500

    [AIC][RFC] promptfoo integration prototype
    Make a custom provider using AIConfig and configure it with promptfoo yaml.
    
    Run (TS connector):
    `npx promptfoo@latest eval -c typescript/scripts/eval/promptfoo/examples/travel/travel_promptfooconfig.yaml`
    
    Run (Py connector):
    `npx promptfoo@latest eval -c python/src/aiconfig/eval/promptfoo/examples/travel/travel_promptfooconfig.yaml`

---
+++ typescript/scripts/eval/promptfoo/run_aiconfig.ts
@@ -0,0 +1,33 @@
+import { config as load_dotenv } from "dotenv";
+import { AIConfigRuntime } from "../../../lib/config";
+import fs from "fs";
+import path from "path";
+
+async function main() {
+  let settings_path = process.argv[2];
+  let question = process.argv[3];
+  let settings = _load_settings(settings_path);
+  let prompt_name = settings["prompt_name"];
+  let aiconfig_path = settings["aiconfig_path"];
+  load_dotenv();
+
+  let fullAIConfigPath = path.join(__dirname, aiconfig_path);
+  let runtime = AIConfigRuntime.load(fullAIConfigPath);
+  let params = {
+    the_query: question,
+  };
+  let result = await runtime.run(prompt_name, params);
+  let final_output: string;
+
+  let r0 = Array.isArray(result) ? result[0] : result;
+  final_output = runtime.getOutputText(prompt_name, r0);
+  console.log(final_output);
+}
+
+function _load_settings(settings_path: string) {
+  let rawdata = fs.readFileSync(settings_path, "utf-8");
+  let settings = JSON.parse(rawdata);
+  return settings;
+}
+
+main();
