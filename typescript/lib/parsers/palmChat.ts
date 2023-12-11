607f57e2d8869fac9fe3e92e9e80b00a1ccfa262
Ankush Pala  ankush@lastmileai.dev <>
2023-12-11 11:48:36 -0500

    Temporary Commit

--- typescript/lib/parsers/palmChat.ts
+++ typescript/lib/parsers/palmChat.ts
@@ -1,4 +1,4 @@
-import { DiscussServiceClient, TextServiceClient, protos } from "@google-ai/generativelanguage";
+import { DiscussServiceClient, protos } from "@google-ai/generativelanguage";
 import { JSONObject } from "../../common";
 import { Prompt, Output, ModelMetadata, ExecuteResult } from "../../types";
 import { AIConfigRuntime } from "../config";
@@ -16,6 +16,8 @@ import { CallbackEvent } from "../callback";
  * Only supports one model: models/text-bison-001
  */
 
+const PALM_CHAT_MODEL_ID = "models/chat-bison-001";
+
 export class PaLMChatParser extends ParameterizedModelParser {
   private client: DiscussServiceClient | null = null;
   _id = "models/chat-bison-001";
@@ -24,7 +26,7 @@ export class PaLMChatParser extends ParameterizedModelParser {
     super();
   }
 
-  public serialize(promptName: string, data: JSONObject, aiConfig: AIConfigRuntime, params?: JSONObject | undefined): Prompt | Prompt[] {
+  public serialize(promptName: string, data: any, aiConfig: AIConfigRuntime, params?: JSONObject | undefined): Prompt | Prompt[] {
     const startEvent = {
       name: "on_serialize_start",
       file: __filename,
@@ -37,27 +39,35 @@ export class PaLMChatParser extends ParameterizedModelParser {
     aiConfig.callbackManager.runCallbacks(startEvent);
 
     // input type was found by looking at the impl of text generation api. When calling textGeneration, step into the defintion of the function and look at the type of the input parameter
-    // ModelParser abstract method types data as JSONObject, but we know that the data is going to be of type protos.google.ai.generativelanguage.v1beta2.IGenerateTextRequest.
-    const input = data as protos.google.ai.generativelanguage.v1beta2.IGenerateTextRequest;
+    // ModelParser abstract method types data as JSONObject, but we know that the data is going to be of type protos.google.ai.generativelanguage.v1beta2.IGenerateMessageRequest.
+    const typedData = data as protos.google.ai.generativelanguage.v1beta2.IGenerateMessageRequest;
+    const modelName = data.model as string;
 
-    const prompt = input.prompt?.text as string;
-    const modelName = input.model as string;
-
-    let modelMetadata: ModelMetadata;
+    let modelMetadata: ModelMetadata = aiConfig.getModelMetadata(_.omit(typedData, ["prompt"]) as JSONObject, modelName);
     // Once relevant attributes are parsed, we no longer need them. These attributes get moved to their respective fields and the rest of the attributes are passed to the model as settings (model metadata).
-    modelMetadata = aiConfig.getModelMetadata(_.omit(input, ["model", "prompt"]) as JSONObject, modelName);
 
-    // Super simple since palm text generation is just one shot prompting.
-    const prompts: Prompt[] = [
-      {
+    // PaLM Chat API supports multi shot.
+    const prompts: Prompt[] = [];
+    const systemPromptTemplate = typedData.prompt?.context || null;
+    // Examples is a an object used by PaLM api to provide example inputs or outputs. If specified in the config, it will be passed forward to the api call.
+    const examples = typedData.prompt?.examples || null;
+    let i = 0;
+    const messagesLength = typedData.prompt?.messages?.length ?? 0;
+    while (i < messagesLength) {
+      //Palm API supports citations. We don't support citations in our config, so we just pass null
+      const promptTemplate = typedData.prompt?.messages?.[i]?.content;
+      // PaLM API mandates a back and forth conversation. Can always expect a message from the user and a response from the model. If there's no latest response, the output will be undefined.
+      const output = typedData.prompt?.messages?.[i + 1]?.content;
+      const prompt: Prompt = {
         name: promptName,
-        input: prompt,
-        metadata: {
-          model: modelMetadata,
-          parameters: params ?? {},
-        },
-      },
-    ];
+        // This input shouldn't need to be type casted because this while loop will only run if there is a message from the user. Not sure why this is happening. TODO: Investigate @ankush-lastmile
+        input: promptTemplate as string,
+        metadata: modelMetadata,
+        outputs: output ? [{ output_type: "execute_result", data: output }] : undefined,
+      };
+      prompts.push(prompt);
+      i += 2;
+    }
 
     const endEvent = {
       name: "on_serialize_end",
@@ -83,18 +93,61 @@ export class PaLMChatParser extends ParameterizedModelParser {
     aiConfig.callbackManager.runCallbacks(event);
 
     const completionParams = this.getModelSettings(prompt, aiConfig) ?? {};
-    completionParams.messages = [];
+    let messages: google.ai.generativelanguage.v1beta2.IMessage[] = [];
+
+    // Get and Set context AKA System Prompt
+    let systemPromptTemplate = prompt.metadata?.context || null;
+    if (systemPromptTemplate) {
+      systemPromptTemplate = this.resolvePromptTemplate(systemPromptTemplate, prompt, aiConfig, params);
+    }
+
+    // Build Chat History
+    // Default to true if remember_chat_context is not specified
+    if (prompt.metadata?.remember_chat_context || true) {
+      for (let i = 0; i < aiConfig.prompts.length; i++) {
+        const previousPrompt = aiConfig.prompts[i];
+        // Stop building chat history if we reach the current prompt
+        if (previousPrompt.name === prompt.name) {
+          break;
+        }
+        // Chat History is only built with prompts from the same model
+        if (aiConfig.getModelName(previousPrompt) === this.id) {
+          // PaLM Api requires a back and forth conversation. If there's no previous output, skip that prompt.
+          if (previousPrompt.outputs!.length > 0) {
+            const promptTemplate = this.getPromptTemplate(prompt, aiConfig);
+            const resolvedPromptTemplate = this.resolvePromptTemplate(promptTemplate, prompt, aiConfig, params);
+
+            const prevPromptOutputText = aiConfig.getOutputText(previousPrompt, aiConfig.getLatestOutput(previousPrompt));
+
+            messages.push({ content: resolvedPromptTemplate, author: "0" });
+            messages.push({ content: prevPromptOutputText, author: "1" });
+          }
+        }
+      }
+    }
+
+    const currentPromptTemplate = this.getPromptTemplate(prompt, aiConfig);
+    const resolvedCurrentPromptTemplate = this.resolvePromptTemplate(currentPromptTemplate, prompt, aiConfig, params);
+    messages.push({ content: resolvedCurrentPromptTemplate, author: "0" });
+
+    completionParams.prompt = {
+      context: systemPromptTemplate,
+      messages,
+      examples: completionParams.examples || null,
+    };
+
+    const refinedCompletionParams = refineGenerateMessageParams(completionParams);
 
     const endEvent = {
       name: "on_deserialize_end",
       file: __filename,
       data: {
-        result: completionParams,
+        result: refinedCompletionParams,
       },
     };
     aiConfig.callbackManager.runCallbacks(endEvent);
 
-    return completionParams;
+    return refinedCompletionParams;
   }
 
   public async run(
@@ -127,7 +180,6 @@ export class PaLMChatParser extends ParameterizedModelParser {
     const [response] = await this.client.generateMessage(completionParams);
 
     const outputs: ExecuteResult[] = constructOutputs(response);
-
     const endEvent = {
       name: "on_run_end",
       file: __filename,
@@ -136,6 +188,8 @@ export class PaLMChatParser extends ParameterizedModelParser {
       },
     } as CallbackEvent;
     await aiConfig.callbackManager.runCallbacks(endEvent);
+
+    prompt.outputs = outputs;
 
     return outputs;
   }
@@ -156,48 +210,44 @@ export class PaLMChatParser extends ParameterizedModelParser {
     }
   }
 
-  public constructConversationHistory(aiconfig: AIConfigRuntime, prompt: Prompt, params: JSONObject): any {
-    if (prompt.metadata!.remember_chat_context) {
-      for (let i = 0; i < aiconfig.prompts.length; i++) {
-        let prevPrompt = aiconfig.prompts[i];
+  //   public constructConversationHistory(aiconfig: AIConfigRuntime, prompt: Prompt, params: JSONObject): any {
+  //     if (prompt.metadata!.remember_chat_context) {
+  //       for (let i = 0; i < aiconfig.prompts.length; i++) {
+  //         let prevPrompt = aiconfig.prompts[i];
 
-        if (prevPrompt.name === prompt.name) {
-          break;
-        }
+  //         if (prevPrompt.name === prompt.name) {
+  //           break;
+  //         }
 
-        if (aiconfig.get_model_name(prevPrompt) === self.id) {
-          
-            const resolvedPrevPrompt = this.resolvePromptTemplate(prevPrompt.input, params, aiConfig);
-          completionParams.messages.push({ content: resolvedPrevPrompt, author: "0" });
+  //         if (aiconfig.get_model_name(prevPrompt) === self.id) {
+  //           const resolvedPrevPrompt = this.resolvePromptTemplate(prevPrompt.input, params, aiConfig);
+  //           completionParams.messages.push({ content: resolvedPrevPrompt, author: "0" });
 
-          const outputText = aiConfig.get_output_text(prevPrompt, aiConfig.get_latest_output(prevPrompt));
-          completionParams["messages"].push({ content: outputText, author: "1" });
-        }
-      }
-    }
-    completionParams["messages"].push({ content: resolvePrompt(prompt, params, aiConfig), author: "0" });
-  }
+  //           const outputText = aiConfig.get_output_text(prevPrompt, aiConfig.get_latest_output(prevPrompt));
+  //           completionParams["messages"].push({ content: outputText, author: "1" });
+  //         }
+  //       }
+  //     }
+  //     completionParams["messages"].push({ content: resolvePrompt(prompt, params, aiConfig), author: "0" });
+  //   }
 }
 
 /**
- *  Refines the completion params for the PALM text generation api. Removes any unsupported params.
- *  The supported keys were found by looking at the PaLM text generation api. `INSERT TYPE HERE`
+ *  Refines the completion params for the PALM chat generation api. Removes any unsupported params.
+ *  The supported keys were found by looking at the PaLM chat api. `INSERT TYPE HERE`
  */
-export function refineTextGenerationParams(params: JSONObject): protos.google.ai.generativelanguage.v1beta2.IGenerateTextRequest {
+export function refineGenerateMessageParams(params: any): protos.google.ai.generativelanguage.v1beta2.IGenerateMessageRequest {
   return {
     model: params.model as string | null,
-    prompt: params.prompt as google.ai.generativelanguage.v1beta2.ITextPrompt | null,
+    prompt: params.prompt as google.ai.generativelanguage.v1beta2.IMessagePrompt | null,
     temperature: params.temperature != null ? (params.temperature as number) : null,
     candidateCount: params.candidateCount != null ? (params.candidateCount as number) : null,
-    maxOutputTokens: params.maxOutputTokens != null ? (params.maxOutputTokens as number) : null,
     topP: params.topP != null ? (params.topP as number) : null,
     topK: params.topK != null ? (params.topK as number) : null,
-    safetySettings: params.safetySettings !== undefined ? (params.safetySettings as google.ai.generativelanguage.v1beta2.ISafetySetting[]) : null,
-    stopSequences: params.stopSequences !== undefined ? (params.stopSequences as string[]) : null,
   };
 }
 
-function constructOutputs(response: protos.google.ai.generativelanguage.v1beta2.IGenerateTextResponse): ExecuteResult[] {
+function constructOutputs(response: protos.google.ai.generativelanguage.v1beta2.IGenerateMessageResponse): ExecuteResult[] {
   if (!response.candidates) {
     return [];
   }
@@ -208,7 +258,7 @@ function constructOutputs(response: protos.google.ai.generativelanguage.v1beta2.
     const candidate = response.candidates[i];
     const output: ExecuteResult = {
       output_type: "execute_result",
-      data: candidate.output,
+      data: candidate.content,
       execution_count: i,
       metadata: _.omit(candidate, ["output"]),
     };
