f184a6216bce95444d28ee566c836970cf318ad4
Rossdan Craig rossdan@lastmileai.dev <>
2023-12-12 02:01:34 -0500

    [hf parser] Add parser for text2Image
    Copying a lot of what I did for the TextGeneration extension: https://github.com/lastmile-ai/aiconfig/tree/main/extensions/HuggingFaceTransformers
    
    Referencing the API defined in
    - https://huggingface.co/docs/diffusers/v0.24.0/en/api/pipelines/auto_pipeline#diffusers.AutoPipelineForText2Image
    - https://huggingface.co/docs/diffusers/using-diffusers/conditional_image_generation
    
    Test by rebasing onto https://github.com/lastmile-ai/aiconfig/pull/461

--- extensions/HuggingFaceTransformers/python/src/aiconfig_extension_hugging_face_transformers/__init__.py
+++ extensions/HuggingFaceTransformers/python/src/aiconfig_extension_hugging_face_transformers/__init__.py
@@ -1,3 +1,4 @@
+from .text_2_image import HuggingFaceText2ImageDiffusor
 from .text_generation import HuggingFaceTextGenerationTransformer
 
-__ALL__ = ["HuggingFaceTextGenerationTransformer"]
+__ALL__ = ["HuggingFaceText2ImageDiffusor", "HuggingFaceTextGenerationTransformer"]
