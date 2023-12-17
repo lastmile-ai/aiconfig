d2109d6a2dc54f742b8e79323ed0645614bbc9a9
Ryan Holinshead <>
2023-12-17 17:37:51 -0500

    [editor] Initial Output Rendering

--- cli/aiconfig-editor/src/shared/types.ts
+++ cli/aiconfig-editor/src/shared/types.ts
@@ -1,7 +1,30 @@
+import { AIConfig, Prompt, Error, ExecuteResult } from "aiconfig";
+
 export type EditorFile = {
   name: string;
   extension: string;
   path: string;
   isDirectory: boolean;
   disabled?: boolean;
+};
+
+export type TextExecuteResultOutput = {
+  type: "text";
+  text: string;
+};
+
+export type ClientExecuteResultOutput = TextExecuteResultOutput; // TODO: Add non-text types
+
+export type ClientExecuteResult = ExecuteResult & {
+  client: ClientExecuteResultOutput;
+};
+
+export type ClientPromptOutput = ClientExecuteResult | Error;
+
+export type ClientPrompt = Omit<Prompt, "outputs"> & {
+  outputs?: ClientPromptOutput[];
+};
+
+export type ClientAIConfig = Omit<AIConfig, "prompts"> & {
+  prompts: ClientPrompt[];
 };
