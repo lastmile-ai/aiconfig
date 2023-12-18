b2710dff58ff6b175da216bdc42d37d7c3f33d88
Ryan Holinshead <>
2023-12-18 17:45:04 -0500

    [editor] Initial Output Rendering
    # [editor] Initial Output Rendering
    
    Get prompt outputs to render. The main consideration here is that we can't really render directly based on the raw AIConfig outputs since those can be any arbitrary data defined by the model parsers. Instead, we add Client-specific types (mainly `ClientExecuteResult`) for the AIConfig which are constructed on load of the config, and which we will be able to properly handle rendering. For non-text outputs, we'll probably have an 'attachments' type to render those output attachments based on mimetype.
    
    Side note -- maybe we just have a single type which can support both text & attachments? We should revisit if we have such models. This should do for now as a starting point.
    
    <img width="2002" alt="Screenshot 2023-12-17 at 5 36 56 PM" src="https://github.com/lastmile-ai/aiconfig/assets/5060851/72d38c2f-59f9-4a63-8f0f-8683aecab75c">

--- cli/aiconfig-editor/src/shared/types.ts
+++ cli/aiconfig-editor/src/shared/types.ts
@@ -1,3 +1,5 @@
+import { AIConfig, Prompt, Error, ExecuteResult } from "aiconfig";
+
 export type EditorFile = {
   name: string;
   extension: string;
@@ -5,3 +7,37 @@ export type EditorFile = {
   isDirectory: boolean;
   disabled?: boolean;
 };
+
+export type TextExecuteResultOutput = {
+  type: "text";
+  text: string;
+};
+
+export type ClientExecuteResultOutput = TextExecuteResultOutput; // TODO: Add non-text types
+
+export type ClientExecuteResult = ExecuteResult & {
+  client: ClientExecuteResultOutput;
+};
+
+export type ClientPromptOutput = ClientExecuteResult | Error;
+
+export type ClientPrompt = Omit<Prompt, "outputs"> & {
+  outputs?: ClientPromptOutput[];
+};
+
+export type ClientAIConfig = Omit<AIConfig, "prompts"> & {
+  prompts: ClientPrompt[];
+};
+
+export function clientConfigToAIConfig(config: ClientAIConfig): AIConfig {
+  return {
+    ...config,
+    prompts: config.prompts.map((prompt) => ({
+      ...prompt,
+      outputs: prompt.outputs?.map((output) => ({
+        ...output,
+        client: undefined,
+      })),
+    })),
+  } as AIConfig;
+}
