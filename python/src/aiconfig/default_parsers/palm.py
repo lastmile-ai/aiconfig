b6fead932407e34a54c950f45d6f83363b7b9b41
Rossdan Craig rossdan@lastmileai.dev <>
2023-12-26 12:40:46 -0500

    [eng week] Set run output type to be list[Output] instead of Output
    Makes it more scalable. The Prompt schema already refers to `outputs` as `List[Output]` anyways

--- python/src/aiconfig/default_parsers/palm.py
+++ python/src/aiconfig/default_parsers/palm.py
@@ -114,7 +114,7 @@ class PaLMTextParser(ParameterizedModelParser):
         aiconfig: "AIConfigRuntime",
         options: InferenceOptions,
         parameters,
-    ) -> Output:
+    ) -> List[Output]:
         """
         Invoked to run a prompt in the .aiconfig. This method should perform
         the actual model inference based on the provided prompt and inference settings.
@@ -313,7 +313,7 @@ class PaLMChatParser(ParameterizedModelParser):
         aiconfig: "AIConfigRuntime",
         options: InferenceOptions,
         parameters,
-    ) -> Output:
+    ) -> List[Output]:
         """
         Invoked to run a prompt in the .aiconfig. This method should perform
         the actual model inference based on the provided prompt and inference settings.
