f1248b5f7913da488771f18b104f3eb6f6c173b1
Rossdan Craig rossdan@lastmileai.dev <>
2023-12-28 11:53:45 -0500

    [python] Use OutputDataWithValue type for function calls
    This is extending https://github.com/lastmile-ai/aiconfig/pull/605 where I converted from model-specific response types --> pure strings. Now I'm going to also support function calls --> OutputData types
    
    Same as last diff (https://github.com/lastmile-ai/aiconfig/pull/610) except now for python files instead of typescript
    
    Don't need to do this for the image outputs since I already did that in https://github.com/lastmile-ai/aiconfig/pull/608
    
    Actually like typescript, the only model parser we need to support for function calling is `openai.py`

--- python/tests/parsers/test_openai_util.py
+++ python/tests/parsers/test_openai_util.py
@@ -2,9 +2,15 @@ import openai
 import pytest
 from aiconfig.Config import AIConfigRuntime
 from aiconfig.default_parsers.openai import refine_chat_completion_params
+from aiconfig.schema import (
+    ExecuteResult,
+    OutputDataWithValue,
+    Prompt,
+    PromptInput,
+    PromptMetadata,
+)
 from mock import patch
 
-from aiconfig import ExecuteResult, Prompt, PromptInput, PromptMetadata
 
 from ..conftest import mock_openai_chat_completion
 from ..util.file_path_utils import get_absolute_file_path_from_relative
@@ -136,10 +142,12 @@ async def test_serialize(set_temporary_env_vars):
                     output_type="execute_result",
                     execution_count=None,
                     data='Hello! How can I assist you today?',
-                    metadata={'rawResponse': {
+                    metadata={
+                        'rawResponse': {
+                            'role': 'assistant',
+                            'content': 'Hello! How can I assist you today?'
+                        },
                         'role': 'assistant',
-                        'content': 'Hello! How can I assist you today?'
-                        }
                     },
                     mime_type=None,
                 )
@@ -277,7 +285,7 @@ async def test_serialize(set_temporary_env_vars):
             ],
         }
 
-        
+
         prompts = await aiconfig.serialize("gpt-3.5-turbo", completion_params, "prompt")
         new_prompt = prompts[1]
         
@@ -330,10 +338,12 @@ async def test_serialize(set_temporary_env_vars):
                     output_type="execute_result",
                     execution_count=None,
                     data="The current weather in Boston is 22 degrees Celsius and sunny.",
-                    metadata={'rawResponse': {
+                    metadata={
+                        'rawResponse': {
+                            'role': 'assistant',
+                            'content': 'The current weather in Boston is 22 degrees Celsius and sunny.',
+                        },
                         'role': 'assistant',
-                        'content': 'The current weather in Boston is 22 degrees Celsius and sunny.',
-                        }
                     },
                     mime_type=None,
                 )
