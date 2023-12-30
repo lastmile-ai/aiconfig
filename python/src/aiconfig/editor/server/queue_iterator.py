ea56f91560f6d57f51b8c8fa8d628c5a571ebb71
Rossdan Craig rossdan@lastmileai.dev <>
2023-12-30 00:57:03 -0500

    Get basic streaming to work for run
    Building upon what Ryan investigated in https://github.com/lastmile-ai/aiconfig/pull/651, all the frontend changes are from him I just rebased onto his PR
    
    
    This is a bit tricky becasue we have to support:
    
    1. streaming models --> use a queue iterator for passing the output text
    2. non-streaming models --> still yield, just don't use a queue iterator and wait for run command to finish
    
    
    General flow:
    1. Parse prompts from client
    2. Define stream callback with queue iterator
    3. Start thread to run aiconfig without blocking main thread from accessing queue iterator
    4. Create a copy of the original AIConfig so we can write partially streamed outputs, yield and display it without risk of race conditions
    5. Wait for queue iterator to start containing data, or wait until max timeout (becuase model may not support streaming)
    5. Iterate through queue iterator, saving output to display config, yield display config
    6. Once output is complete, wait for the original `config.run()` thread and display the output from that
    
    Open questions/TODOs
    1. [solved - use t.How can we check whether model supports streaming or not? Right now we just default to having a max timeout of 5s, but long-term would be better for people to explicitly mark this as a boolean flag in their model parser class
    2. I need update the output format for streaming. I thought it was fine but guess not, will verify again. Not ideal but also not a crazy blocker for now
    3. Client needs to also support streaming, but that's fine Ryan can get unblocked with this diff now
    4. Pretty complex, but streaming will break for `run_with_dependencies`. I've got a proposal to fix forward in https://github.com/lastmile-ai/gradio-workbook/pull/64 and really want people to take a look and give feedback
    
    ## Test plan
    ```bash
    alias aiconfig="python -m 'aiconfig.scripts.aiconfig_cli'"
    aiconfig edit --aiconfig-path="/Users/rossdancraig/Projects/aiconfig/cookbooks/Getting-Started/travel.aiconfig.json" --server-port=8080 --server-mode=debug_servers
    
    # Now run this from another terminal
    curl http://localhost:8080/api/run -d '{"prompt_name":"get_activities"}' -X POST -H 'Content-Type: application/json'
    ```
    
    I also added this line to print output:
    ```
    print(accumulated_output_text)
    ```
    
    | Streaming | Non-streaming (you can refresh page to short-circuit and get AIConfig) | Non-streaming (normal waiting) |
    |---|---|---|
    |  https://github.com/lastmile-ai/aiconfig/assets/151060367/446fe008-9f34-4515-bb80-7b317ab15bd6 | https://github.com/lastmile-ai/aiconfig/assets/151060367/d94b636a-2703-49da-a729-82c7a5f1079a | https://github.com/lastmile-ai/aiconfig/assets/151060367/5195e23a-cade-44ac-852e-bdd0bfe374a4 |

---
+++ python/src/aiconfig/editor/server/queue_iterator.py
@@ -0,0 +1,41 @@
+"""Queue iterator for streaming. Can only process strings for now
+but in future will try to make it generic"""
+# import asyncio
+from queue import Queue
+# from typing import Generic, TypeVar
+
+# # TODO: Add generic typing for queue items
+# # (couldn't get sentinel value to work with generics)
+# T = TypeVar('T')
+STOP_STREAMING_SIGNAL = object() #sentinel value to indicate end of stream
+
+class QueueIterator():
+    """In order to support text streaming, we need to store
+    the output in a queue and iterate over those values. A lot of this was
+    inspired by HuggingFace's TextIteratorStreamer object:
+    
+    I know I can just use a queue directly in the callsite with 
+    `iter(queue.get, None)`, but having a class makes it easier to manage 
+    and abstracts it a bit more.
+    """
+    def __init__(self):
+        self.q = Queue()
+        self.stop_signal = STOP_STREAMING_SIGNAL
+        self.timeout = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        value = self.q.get(block=True, timeout=self.timeout)
+        if value == self.stop_signal:
+            raise StopIteration()
+        return value
+
+    def put(self, text: str, stream_end: bool = False):
+        self.q.put(text, timeout=self.timeout)
+        if stream_end:
+            self.q.put(self.stop_signal, timeout=self.timeout)
+
+    def isEmpty(self) -> bool:
+        return self.q.empty()
