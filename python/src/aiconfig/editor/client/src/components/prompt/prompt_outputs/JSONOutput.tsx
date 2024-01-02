23c951a6f5bd21d88643ca5ec2176af6057f8595
Ryan Holinshead <>
2024-01-02 12:14:25 -0500

    [editor] JSON Renderer for Prompt Input
    # [editor] JSON Renderer for Prompt Input
    
    Add a toggle-able JSON editor for prompt inputs so that users can see/modify the input JSON directly. This sets up the JSONEditor component which can be reused in other places (e.g. prompt model settings). This component takes in a schema object to provide validation of the input -- we should investigate and update the implementation to just take a reference to a definition in our config schema instead of needing to duplicate the schema definition for PromptInput but I wasn't able to get that working within a reasonable time box.
    
    
    https://github.com/lastmile-ai/aiconfig/assets/5060851/c5e14ac0-0771-41f5-adea-4369b886ef00

--- python/src/aiconfig/editor/client/src/components/prompt/prompt_outputs/JSONOutput.tsx
+++ python/src/aiconfig/editor/client/src/components/prompt/prompt_outputs/JSONOutput.tsx
@@ -1,11 +1,7 @@
-import { Prism } from "@mantine/prism";
 import { JSONValue } from "aiconfig";
 import { memo } from "react";
+import JSONRenderer from "../../JSONRenderer";
 
 export default memo(function JSONOutput({ content }: { content: JSONValue }) {
-  return (
-    <Prism language="json" styles={{ code: { textWrap: "pretty" } }}>
-      {JSON.stringify(content, null, 2)}
-    </Prism>
-  );
+  return <JSONRenderer content={content} />;
 });
