d64d83993900781d745db35bb2f4f7ead9137ac7
Ryan Holinshead <>
2024-01-02 19:22:45 -0500

    [editor] Add DalleImageGenerationParserPromptSchema
    # [editor] Add DalleImageGenerationParserPromptSchema
    
    Add the prompt schema for dall-e-2 to match `/opt/homebrew/Caskroom/miniconda/base/envs/aiconfig/lib/python3.12/site-packages/openai/resources/images.py`:
    
    ```
     def generate(
            self,
            *,
            prompt: str,
            model: Union[str, Literal["dall-e-2", "dall-e-3"], None] | NotGiven = NOT_GIVEN,
            n: Optional[int] | NotGiven = NOT_GIVEN,
            quality: Literal["standard", "hd"] | NotGiven = NOT_GIVEN,
            response_format: Optional[Literal["url", "b64_json"]] | NotGiven = NOT_GIVEN,
            size: Optional[Literal["256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"]] | NotGiven = NOT_GIVEN,
            style: Optional[Literal["vivid", "natural"]] | NotGiven = NOT_GIVEN,
            user: str | NotGiven = NOT_GIVEN,
            # Use the following arguments if you need to pass additional parameters to the API that aren't available via kwargs.
            # The extra values given here take precedence over values defined on the client or passed to this method.
            extra_headers: Headers | None = None,
            extra_query: Query | None = None,
            extra_body: Body | None = None,
            timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
        ) -> ImagesResponse:
    ```
    
    And supported keys specified in dalle.py:
    ```
    supported_keys = {"model", "n", "quality", "response_format", "size", "style"}
    ```
    
    
    <img width="1235" alt="Screenshot 2024-01-02 at 7 20 29 PM" src="https://github.com/lastmile-ai/aiconfig/assets/5060851/bf92287b-a56d-4711-8339-d9b59d4c3564">

---
+++ python/src/aiconfig/editor/client/src/shared/prompt_schemas/DalleImageGenerationParserPromptSchema.ts
@@ -0,0 +1,36 @@
+import { PromptSchema } from "../../utils/promptUtils";
+
+export const DalleImageGenerationParserPromptSchema: PromptSchema = {
+  // See /opt/homebrew/Caskroom/miniconda/base/envs/aiconfig/lib/python3.12/site-packages/openai/resources/images.py
+  // for supported settings
+
+  input: {
+    type: "string",
+  },
+  model_settings: {
+    type: "object",
+    properties: {
+      n: {
+        type: "integer",
+        minimum: 1,
+        maximum: 10,
+      },
+      quality: {
+        type: "string",
+        enum: ["standard", "hd"],
+      },
+      response_format: {
+        type: "string",
+        enum: ["url", "b64_json"],
+      },
+      size: {
+        type: "string",
+        enum: ["256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"],
+      },
+      style: {
+        type: "string",
+        enum: ["vivid", "natural"],
+      },
+    },
+  },
+};
