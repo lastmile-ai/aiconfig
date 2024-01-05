3846685217340a51ba346cb4274f68ec24805a53
Ankush Pala  ankush@lastmileai.dev <>
2024-01-05 14:47:20 -0500

    [extensions][py][hf] 2/n ASR model parser impl
    ## Testplan
    
    Created an mp3 file that says "hi". Used aiconfig to run asr on it.
    
    |<img width="596" alt="Screenshot 2024-01-05 at 2 39 17 PM" src="https://github.com/lastmile-ai/aiconfig/assets/141073967/42c7ddbe-20ca-4828-b609-725b88a08939">|<img width="900" alt="Screenshot 2024-01-05 at 2 41 04 PM" src="https://github.com/lastmile-ai/aiconfig/assets/141073967/74b21333-d347-4270-bbd6-efd318785172">|
    | ------------- | ------------- |

--- extensions/HuggingFace/python/src/aiconfig_extension_hugging_face/local_inference/automatic_speech_recognition.py
+++ extensions/HuggingFace/python/src/aiconfig_extension_hugging_face/local_inference/automatic_speech_recognition.py
@@ -1,8 +1,8 @@
-from typing import Any, Coroutine, Dict, Optional, List, TYPE_CHECKING
-from aiconfig import ParameterizedModelParser, InferenceOptions, AIConfig
-
-from aiconfig.schema import Prompt, Output
-from transformers import Pipeline
+from typing import Any, Dict, Optional, List, TYPE_CHECKING
+from aiconfig import ParameterizedModelParser, InferenceOptions
+import torch
+from aiconfig.schema import Prompt, Output, ExecuteResult, Attachment
+from transformers import pipeline, Pipeline
 
 if TYPE_CHECKING:
     from aiconfig import AIConfigRuntime
@@ -24,7 +24,7 @@ class HuggingFaceAutomaticSpeechRecognition(ParameterizedModelParser):
                 config.register_model_parser(parser)
         """
         super().__init__()
-        self.generators: dict[str, Pipeline] = {}
+        self.pipelines: dict[str, Pipeline] = {}
 
     def id(self) -> str:
         """
@@ -56,10 +56,70 @@ class HuggingFaceAutomaticSpeechRecognition(ParameterizedModelParser):
     async def deserialize(
         self,
         prompt: Prompt,
-        aiconfig: "AIConfig",
+        aiconfig: "AIConfigRuntime",
         params: Optional[Dict[str, Any]] = {},
     ) -> Dict[str, Any]:
-        pass
+        # Build Completion data
+        completion_params = self.get_model_settings(prompt, aiconfig)
+
+        attachment = retrieve_first_attachment_and_check_audio_or_throw(prompt)
+
+        # only one input is supported
+        inputs = attachment.data
+
+        completion_params["inputs"] = inputs
+        return completion_params
 
     async def run_inference(self, prompt: Prompt, aiconfig: "AIConfigRuntime", options: InferenceOptions, parameters: Dict[str, Any]) -> list[Output]:
-        pass
+        model_name = aiconfig.get_model_name(prompt)
+
+        if isinstance(model_name, str) and model_name not in self.pipelines:
+            device = self._get_device()
+            self.pipelines[model_name] = pipeline(task="automatic-speech-recognition", model=model_name, device=device)
+
+        asr_pipeline = self.pipelines[model_name]
+        completion_data = await self.deserialize(prompt, aiconfig, parameters)
+
+        response = asr_pipeline(**completion_data)
+
+        output = ExecuteResult(output_type="execute_result", data=response, metadata={})
+
+        prompt.outputs = [output]
+
+        return prompt.outputs
+
+    def _get_device(self) -> str:
+        if torch.cuda.is_available():
+            return "cuda"
+        # Mps backend is not supported for all asr models.
+        # This is currently a torch library limitation. Test this by creating a pipeline with mps backend.
+        return "cpu"
+
+    def get_output_text(self, response: dict[str, Any]) -> str:
+        return
+
+
+def retrieve_first_attachment_and_check_audio_or_throw(prompt: Prompt) -> Attachment:
+    """
+    Retrieves the first attachment from the prompt input and checks if it is an audio attachment.
+
+    Throws an exception if
+    - First attachment is not audio
+    - no attachments are found
+    - operation fails for any reason
+    """
+    if not hasattr(prompt.input, "attachments"):
+        raise ValueError(f"No attachments found in input for prompt {prompt.name}. Please add an audio attachment to the prompt input.")
+
+    if len(prompt.input.attachments) == 0:
+        raise ValueError("No attachments found in input.")
+
+    attachment = prompt.input.attachments[0]
+
+    if not hasattr(attachment, "mime_type"):
+        raise ValueError(f"Attachment {attachment} has no mime type. Specify the audio mimetype in the config")
+
+    if not attachment.mime_type.startswith("audio/"):
+        raise ValueError(f"Invalid attachment type {attachment.mime_type}. Expected audio.")
+
+    return attachment
