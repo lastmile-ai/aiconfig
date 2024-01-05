c3ef24c906299069a0ef46519193d85aebd60870
Jonathan Lessinger <jonathan@lastmileai.dev>
2024-01-05 18:36:57 -0500

    [AIC-py] add hf text 2 speech parser
    ```
    {
      "name": "NYC Trip Planner",
      "description": "Intrepid explorer with ChatGPT and AIConfig",
      "schema_version": "latest",
      "metadata": {
        "models": {
          "gpt2": {
            "model": "gpt2",
            "max_tokens": 3000,
            "system_prompt": "Some random system prompt"
          },
          "suno/bark": {
            "model": "suno/bark"
          }
        },
        "model_parsers": {
          "suno/bark": "HuggingFaceText2SpeechTransformer"
        },
        "default_model": "gpt2"
      },
      "prompts": [
        {
          "name": "test_tts",
          "input": "the quick brown fox",
          "metadata": {
            "model": {
              "name": "suno/bark",
              "settings": {}
            }
          }
        }
      ]
    }
    
    
        mp = HuggingFaceText2SpeechTransformer()
        ModelParserRegistry.register_model_parser(mp)
    
        config = AIConfigRuntime.load("/Users/jonathan/Projects/aiconfig/test_hf_t2s.aiconfig.json")
        config.callback_manager = CallbackManager([])
    
        options = InferenceOptions(stream=False)
        out = await config.run("test_tts", options=options)
        with open("test.wav", "wb") as f:
            f.write(_b64_decode_str(out[0].data.value))
    
    
    Listened to test.wav, it is in fact someone saying "the quick brown fox".
    
    ```

--- extensions/HuggingFace/python/src/aiconfig_extension_hugging_face/__init__.py
+++ extensions/HuggingFace/python/src/aiconfig_extension_hugging_face/__init__.py
@@ -1,10 +1,7 @@
 from .local_inference.text_2_image import HuggingFaceText2ImageDiffusor
 from .local_inference.text_generation import HuggingFaceTextGenerationTransformer
-from .remote_inference_client.text_generation import HuggingFaceTextGenerationClient
 from .local_inference.text_summarization import HuggingFaceTextSummarizationTransformer
 from .local_inference.text_translation import HuggingFaceTextTranslationTransformer
-
-# from .remote_inference_client.text_generation import HuggingFaceTextGenerationClient
 
 LOCAL_INFERENCE_CLASSES = [
     "HuggingFaceText2ImageDiffusor",
