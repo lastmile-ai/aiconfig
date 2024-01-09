70327eede38f1aeb0d05ba9955e4af86c3d7e246
Ryan Holinshead <>
2024-01-09 16:37:50 -0500

    [editor][wip] Packaging AIConfigEditor Component

---
+++ python/src/aiconfig/editor/client/editor_package/src/components/index.ts
@@ -0,0 +1,8 @@
+// For now, just export the AIConfigEditor component and relevant types.
+export { AIConfigEditor } from "./AIConfigEditor";
+export type {
+  AIConfigCallbacks,
+  RunPromptStreamCallback,
+  RunPromptStreamErrorCallback,
+  RunPromptStreamErrorEvent,
+} from "./AIConfigEditor";
