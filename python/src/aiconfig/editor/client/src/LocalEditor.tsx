2322f5e80e25b78c08cef4b50733e96ab469a28f
Sarmad Qadri <sarmad@lastmileai.dev>
2024-01-12 14:02:22 -0500

    [WIP] Clean up unnecessary duplicate of aiconfig editor

--- python/src/aiconfig/editor/client/src/LocalEditor.tsx
+++ python/src/aiconfig/editor/client/src/LocalEditor.tsx
@@ -12,16 +12,53 @@ import {
   Output,
   Prompt,
 } from "aiconfig";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { ufetch } from "ufetch";
 import { ROUTE_TABLE } from "./utils/api";
 import { streamingApiChain } from "./utils/oboeHelpers";
+import WebviewContext from "./WebviewContext";
 
 export default function Editor() {
-  const [aiconfig, setAiConfig] = useState<AIConfig | undefined>();
+  const [aiconfig, setAIConfig] = useState<AIConfig | undefined>();
+
+  const { vscode } = useContext(WebviewContext);
+
+  const updateContent = useCallback(
+    async (text: string) => {
+      // TODO: saqadri - this won't work for YAML -- the handling of the text needs to include the logic from AIConfig.load
+      const updatedConfig = text != null ? JSON.parse(text) : {};
+      console.log("updatedConfig=", JSON.stringify(updatedConfig));
+      setAIConfig(updatedConfig);
+
+      // Then persist state information.
+      // This state is returned in the call to `vscode.getState` below when a webview is reloaded.
+      vscode?.setState({ text });
+
+      // TODO: saqadri - as soon as content is updated, we have to call /load endpoint for the server to have the latest content as well
+      // However, instead of loading from FS, the /load endpoint should load from the data passed to it here.
+    },
+    [vscode]
+  );
+
+  // Handle messages sent from the extension to the webview
+  window.addEventListener("message", (event) => {
+    console.log("onMessage, event=", JSON.stringify(event));
+    const message = event.data; // The json data that the extension sent
+    switch (message.type) {
+      case "update": {
+        console.log("onMessage, message=", JSON.stringify(message));
+        const text = message.text;
+
+        // Update our webview's content
+        updateContent(text);
+        return;
+      }
+    }
+  });
 
   // const loadConfig = useCallback(async () => {
   //   const res = await ufetch.post(ROUTE_TABLE.LOAD, {});
+
   //   setAiConfig(res.aiconfig);
   // }, []);
 
