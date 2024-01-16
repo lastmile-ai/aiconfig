ffcab729c2a8f5d5d6ac60b61fd681a230de11c4
Rossdan Craig rossdan@lastmileai.dev <>
2024-01-16 09:20:25 -0500

    [4/n] Read-only mode: Connect context to ClearOutputsButton
    Created a new component because it's easier and cleaner than having a ternary inlined based on read-only prompt
    
    
    ## Test Plan
    Change `readOnly = false` to `readOnly = true`
    Before
    <img width="1512" alt="Screenshot 2024-01-16 at 08 52 46" src="https://github.com/lastmile-ai/aiconfig/assets/151060367/0fdb11b4-e618-4e93-a1d4-261597d99680">
    
    
    After
    <img width="1512" alt="Screenshot 2024-01-16 at 09 17 16" src="https://github.com/lastmile-ai/aiconfig/assets/151060367/051fbbbf-198f-4130-94fc-7c02b104cd6c">

---
+++ python/src/aiconfig/editor/client/src/components/ClearOutputsButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@mantine/core";
+import { memo, useContext } from "react";
+
+import AIConfigContext from "../contexts/AIConfigContext";
+
+type Props = {
+  onClearOutputs: () => void;
+};
+
+export default memo(function ClearOutputsButton({ onClearOutputs }: Props) {
+  const { readOnly } = useContext(AIConfigContext);
+
+  return readOnly ? (
+    <Button disabled size="xs" variant="gradient">
+      Clear Outputs
+    </Button>
+  ) : (
+    <Button
+      loading={undefined}
+      onClick={onClearOutputs}
+      size="xs"
+      variant="gradient"
+    >
+      Clear Outputs
+    </Button>
+  );
+});
