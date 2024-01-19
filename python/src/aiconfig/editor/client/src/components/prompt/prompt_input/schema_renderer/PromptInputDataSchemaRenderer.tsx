824a479225d0bed450b0bcf4173d60f2471044cb
Ankush Pala  ankush@lastmileai.dev <>
2024-01-18 19:51:57 -0500

    8/n Read-only mode: Input rendering
    Attempted to add read-only functionality wherever applicable to input renderings.
    
    ## Testplan
    
    | ![image1](https://github.com/lastmile-ai/aiconfig/assets/141073967/31405238-bbd1-42c0-a5fc-58102fe05021) | ![image2](https://github.com/lastmile-ai/aiconfig/assets/141073967/bb8d6543-25d0-4b61-826b-ded9ddab4295) |
    | --- | --- |
    | ![image1](https://github.com/lastmile-ai/aiconfig/assets/141073967/dcec6c8b-fbe8-4aea-b2f1-f55153866385) | ![image2](https://github.com/lastmile-ai/aiconfig/assets/141073967/8e90c287-3304-484e-8831-496b0f5cd2a8) |
    | ![image1](https://github.com/lastmile-ai/aiconfig/assets/141073967/38e2fc27-bb39-4143-8dc1-67876110f1c6) | ![image2](https://github.com/lastmile-ai/aiconfig/assets/141073967/34d4db13-74b5-4617-bb5c-5c851e41afb8) |
    | ![image1](https://github.com/lastmile-ai/aiconfig/assets/141073967/80953234-3a0c-4aa5-bfc5-7d7c9176be58) | ![image2](https://github.com/lastmile-ai/aiconfig/assets/141073967/4d66ec78-04f8-45ae-b396-a91e8f3355d3) |
    
    ## dependencies
    
    built ontop of #961

--- python/src/aiconfig/editor/client/src/components/prompt/prompt_input/schema_renderer/PromptInputDataSchemaRenderer.tsx
+++ python/src/aiconfig/editor/client/src/components/prompt/prompt_input/schema_renderer/PromptInputDataSchemaRenderer.tsx
@@ -1,7 +1,9 @@
 import { PromptInputObjectDataSchema } from "../../../../utils/promptUtils";
-import { Textarea } from "@mantine/core";
+import { Spoiler, Textarea } from "@mantine/core";
 import { JSONValue } from "aiconfig";
-import { memo } from "react";
+import { memo, useContext } from "react";
+import AIConfigContext from "../../../../contexts/AIConfigContext";
+import { TextRenderer } from "../../TextRenderer";
 
 type Props = {
   schema: PromptInputObjectDataSchema;
@@ -9,20 +11,27 @@ type Props = {
   onChangeData: (value: JSONValue) => void;
 };
 
-export default memo(function PromptInputDataSchemaRenderer({
-  schema,
-  data,
-  onChangeData,
-}: Props) {
+export default memo(function PromptInputDataSchemaRenderer({ schema, data, onChangeData }: Props) {
+  const { readOnly } = useContext(AIConfigContext);
+
   switch (schema.type) {
-    case "string":
-      return (
+    case "string": {
+      const valueData = data ? (data as string) : "";
+      return readOnly ? (
+        <div style={{ padding: "0.5em" }}>
+          <Spoiler maxHeight={200} showLabel="Show more" hideLabel="Hide" initialState={false} transitionDuration={300}>
+            <TextRenderer content={valueData} />
+          </Spoiler>
+        </div>
+      ) : (
         <Textarea
-          value={data ? (data as string) : ""}
+          value={valueData}
           onChange={(e) => onChangeData(e.target.value)}
-          placeholder="Type a prompt"
+          disabled={readOnly}
+          placeholder={readOnly ? "" : "Type a prompt"}
         />
       );
+    }
     default:
       return null; // TODO: Handle other input.data types
   }
