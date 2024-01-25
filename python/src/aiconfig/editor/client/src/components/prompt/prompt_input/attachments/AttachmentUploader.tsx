f326828a4f061201bd65a860da91bf24b29159b8
Rossdan Craig rossdan@lastmileai.dev <>
2024-01-25 17:47:54 -0500

    Share button [2/n]: Move uploadFile function into a utils file
    Going to re-use this next PR for uploading AIConfig files to S3
    
    ## Test Plan
    No functional changes, make sure that uploading files still works for ASR and Image 2 text
    
    Go to `aiconfig/python/src/aiconfig/editor/client` and run this command:
    ```
    rm -rf node_modules && yarn && yarn build
    ```
    
    Then go to `aiconfig` dir and run this command:
    ```
    aiconfig_path  =./cookbooks/Gradio/huggingface.aiconfig.json
    parsers_path=./cookbooks/Gradio/hf_model_parsers.py
    aiconfig edit --aiconfig-path=$aiconfig_path --server-port=8080 --server-mode=debug_servers --parsers-module-path=$parsers_path
    ```
    
    https://github.com/lastmile-ai/aiconfig/assets/151060367/7a5141f1-bb1b-496c-b039-724a8eeb11dd

--- python/src/aiconfig/editor/client/src/components/prompt/prompt_input/attachments/AttachmentUploader.tsx
+++ python/src/aiconfig/editor/client/src/components/prompt/prompt_input/attachments/AttachmentUploader.tsx
@@ -1,77 +1,21 @@
 import { memo, useContext, useState } from "react";
 import type { Attachment, Attachment as InputAttachment } from "aiconfig";
-import { PromptInputObjectAttachmentsSchema } from "../../../../utils/promptUtils";
+import { ActionIcon, Container, Text, Title, Tooltip } from "@mantine/core";
 import { Dropzone, FileRejection } from "@mantine/dropzone";
+import { IconX } from "@tabler/icons-react";
+import AIConfigContext from "../../../../contexts/AIConfigContext";
 import {
   UploadState,
   getDropzoneUploadErrorMessage,
 } from "../../../../utils/dropzoneHelpers";
-import { ActionIcon, Container, Text, Title, Tooltip } from "@mantine/core";
-import { IconX } from "@tabler/icons-react";
-import AIConfigContext from "../../../../contexts/AIConfigContext";
+import { PromptInputObjectAttachmentsSchema } from "../../../../utils/promptUtils";
+import { uploadFileToS3 } from "../../../../utils/uploadFileToS3";
 
 type Props = {
   schema: PromptInputObjectAttachmentsSchema;
   onUploadAttachments: (attachments: InputAttachment[]) => void;
   onCancel?: () => void;
 };
-
-// s3 file uris cannot have '+' character, so replace with '_'
-function sanitizeFileName(name: string) {
-  return name.replace(/[_+]/g, "_");
-}
-
-export function getTodayDateString(): string {
-  const date = new Date();
-  const dateString = `${date.getFullYear()}_${
-    date.getMonth() + 1
-  }_${date.getDate()}`;
-  const timeString = `${date.getUTCHours()}_${date.getUTCMinutes()}_${date.getUTCSeconds()}`;
-  return `${dateString}_${timeString}`;
-}
-
-// TODO: Make this configurable for external deployments
-async function uploadFile(file: File): Promise<{ url: string }> {
-  const randomPath = Math.round(Math.random() * 10000);
-  // TODO: Add back once CORS is resolved
-  // const policyResponse = await fetch(
-  //   "https://lastmileai.dev/api/upload/publicpolicy"
-  // );
-  // const policy = await policyResponse.json();
-  const uploadUrl = "https://s3.amazonaws.com/lastmileai.aiconfig.public/";
-  const uploadKey = `uploads/${getTodayDateString()}/${randomPath}/${sanitizeFileName(
-    file.name
-  )}`;
-
-  const formData = new FormData();
-  formData.append("key", uploadKey);
-  formData.append("acl", "public-read");
-  formData.append("Content-Type", file.type);
-  // formData.append("AWSAccessKeyId", policy.AWSAccessKeyId);
-  formData.append("success_action_status", "201");
-  // formData.append("Policy", policy.s3Policy);
-  // formData.append("Signature", policy.s3Signature);
-  formData.append("file", file);
-
-  // See this about changing to use XMLHTTPRequest to show upload progress as well
-  // https://medium.com/@cpatarun/tracking-file-upload-progress-to-amazon-s3-from-the-browser-71be6712c63d
-  const rawRes = await fetch(uploadUrl, {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    body: formData,
-    headers: {
-      Authorization: "",
-    },
-  });
-
-  if (rawRes.ok && rawRes.status === 201) {
-    // Dont really need to parse xml s3 response, just use the keys, etc. that were passed
-    return { url: `${uploadUrl}${uploadKey}` };
-  } else {
-    throw new Error("Error uploading to S3!");
-  }
-}
 
 function getSupportedFileTypes(schema: PromptInputObjectAttachmentsSchema) {
   return schema.items.mime_types.join(", ");
@@ -86,7 +30,7 @@ Props) {
   const [fileList, setFileList] = useState<File[]>([]);
   const [uploadState, setUploadState] = useState<UploadState>("idle");
   const [uploadError, setUploadError] = useState<string | null>(null);
-  const {readOnly} = useContext(AIConfigContext);
+  const { readOnly } = useContext(AIConfigContext);
 
   const onDropzoneClick = async (files: File[]) => {
     if (uploadState === "dropzone_error") {
@@ -100,7 +44,7 @@ Props) {
         setUploadState("uploading");
         uploads = await Promise.all(
           files.map(async (file: File) => {
-            const upload = await uploadFile(file);
+            const upload = await uploadFileToS3(file);
             return {
               url: upload.url,
               mimeType: file.type,
@@ -117,7 +61,7 @@ Props) {
 
       const attachments: Attachment[] = uploads.map((upload) => {
         return {
-          data: {value: upload.url, kind: "file_uri"},
+          data: { value: upload.url, kind: "file_uri" },
           mime_type: upload.mimeType,
         };
       });
