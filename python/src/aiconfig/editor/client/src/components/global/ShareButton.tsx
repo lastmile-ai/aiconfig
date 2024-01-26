cc17dabae54db7a847aae9f97d9967ab7d441301
Rossdan Craig rossdan@lastmileai.dev <>
2024-01-26 17:00:25 -0500

    Share Button [7/n]: Display loading UI
    Created a separate component for the shareButton to manage the `isLoading` state and show that in the UI
    
    ## Test Plan
    Rebase on then do: https://github.com/lastmile-ai/aiconfig/pull/1045
    
    Go to `aiconfig/python/src/aiconfig/editor/client` and run this command:
    ```
    rm -rf node_modules && yarn && yarn build
    ```
    
    Then go to `aiconfig` dir and run this command:
    ```
    aiconfig_path  =./cookbooks/Gradio/huggingface.aiconfig.json
    parsers_path=./cookbooks/Gradio/hf_model_parsers.py
    aiconfig edit --aiconfig-path=$aiconfig_path --server-port=8080 --server-mode=debug_servers --parsers-module-path=$parsers_path
    ```
    
    No Error Testing
    
    https://github.com/lastmile-ai/aiconfig/assets/151060367/0c177c59-54b8-4d04-adfa-aea6d78f5f07
    
    Error Testing
    
    https://github.com/lastmile-ai/aiconfig/assets/151060367/1abcce37-8ba4-4513-b826-ea0f9d7bb720

---
+++ python/src/aiconfig/editor/client/src/components/global/ShareButton.tsx
@@ -0,0 +1,50 @@
+import { Button, Flex, Loader, Tooltip } from "@mantine/core";
+import { IconPlayerPlayFilled, IconPlayerStop } from "@tabler/icons-react";
+import { memo, useContext, useState } from "react";
+import AIConfigContext from "../../contexts/AIConfigContext";
+
+type Props = {
+  onShare: () => Promise<string | void>;
+};
+
+export default memo(function ShareButton({ onShare }: Props) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onClick = async () => {
+    if (isLoading) {
+      return;
+    }
+
+    setIsLoading(true);
+    const shareUrl: string | void = await onShare();
+    setIsLoading(false);
+
+    if (!shareUrl) {
+      return;
+    }
+
+    console.log("Share URL: ", shareUrl);
+  };
+
+  const button = (
+    <Button
+      loading={isLoading}
+      disabled={isLoading}
+      onClick={onClick}
+      p="xs"
+      size="xs"
+      variant="filled"
+    >
+      Share
+    </Button>
+  );
+
+  const tooltipMessage: string = isLoading
+    ? "Generating share link..."
+    : "Create a link to share your AIConfig!";
+  return (
+    <Tooltip label={tooltipMessage} withArrow>
+      <div>{button}</div>
+    </Tooltip>
+  );
+});
