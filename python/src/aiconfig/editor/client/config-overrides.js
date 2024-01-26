34df2ee1bc3be05b5c29fea4cb72a1cfc5dfc3fc
Ryan Holinshead <>
2024-01-26 09:41:45 -0500

    Set Up dev 'watch' for aiconfig-editor declarations HMR

--- python/src/aiconfig/editor/client/config-overrides.js
+++ python/src/aiconfig/editor/client/config-overrides.js
@@ -2,4 +2,37 @@ const { alias, configPaths } = require("react-app-rewire-alias");
 
 const aliasMap = configPaths("./tsconfig.paths.json");
 
-module.exports = alias(aliasMap);
+module.exports = {
+  webpack: function (config) {
+    const webpackConfig = alias(aliasMap)(config);
+    return {
+      ...webpackConfig,
+      watchOptions: {
+        ...webpackConfig.watchOptions,
+        followSymlinks: true,
+      },
+      resolve: {
+        ...webpackConfig.resolve,
+        symlinks: false,
+      },
+    };
+  },
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      const devConfig = configFunction(proxy, allowedHost);
+      return {
+        ...devConfig,
+        watchFiles: {
+          paths: [
+            "src/**/*",
+            "node_modules/aiconfig-editor/**/*",
+            "node_modules/@lastmileai/aiconfig-editor/**/*",
+          ],
+          options: {
+            followSymlinks: true,
+          },
+        },
+      };
+    };
+  },
+};
