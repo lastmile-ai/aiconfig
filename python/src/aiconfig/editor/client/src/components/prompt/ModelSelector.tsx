2e62f5f4552f65edd8812859ede1e04b2c2c9ef8
Ryan Holinshead <>
2024-01-26 16:32:54 -0500

    Set Model Selector Label to 'HuggingFace Task' for 'gradio' Mode

--- python/src/aiconfig/editor/client/src/components/prompt/ModelSelector.tsx
+++ python/src/aiconfig/editor/client/src/components/prompt/ModelSelector.tsx
@@ -19,6 +19,7 @@ export default memo(function ModelSelector({
   onSetModel,
   defaultConfigModelName,
 }: Props) {
+  const { mode } = useContext(AIConfigContext);
   const { readOnly } = useContext(AIConfigContext);
   const [selectedModel, setSelectedModel] = useState<string | undefined>(
     getPromptModelName(prompt, defaultConfigModelName)
@@ -43,7 +44,7 @@ export default memo(function ModelSelector({
       placeholder="Select model"
       limit={100}
       className="ghost"
-      label="Model"
+      label={mode === "gradio" ? "HuggingFace Task" : "Model"}
       variant="unstyled"
       maxDropdownHeight={200}
       disabled={readOnly}
