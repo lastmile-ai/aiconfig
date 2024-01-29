7ddfc1d632482ded73209615661d15f97ad72f3e
Rossdan Craig rossdan@lastmileai.dev <>
2024-01-29 12:48:25 -0500

    Download Button [1/n]: Setup UI on the editor client
    If the download callback is defined (not for local editor or VSCode, but will be for Gradio), then we display it and use the loading state to disable you from clicking on the button.
    
    I'm making the signature of the callback take in an AIConfig object parameter, because we have function `clientConfigToAIConfig` within the editor pacakge: https://github.com/lastmile-ai/aiconfig/blob/c838856f36941b0796c8f12edcf846cc18ebdaa1/python/src/aiconfig/editor/client/src/shared/types.ts#L37
    
    ## Test Plan
    Rebase onto https://github.com/lastmile-ai/aiconfig/pull/1045
    
    Go to `aiconfig/python/src/aiconfig/editor/client` and run this command:
    ```
    rm -rf node_modules && yarn && yarn build
    ```
    
    Then go to `aiconfig` dir and run this command:
    ```
    aiconfig_path=./cookbooks/Gradio/huggingface.aiconfig.json
    parsers_path=./cookbooks/Gradio/hf_model_parsers.py
    aiconfig edit --aiconfig-path=$aiconfig_path --server-port=8080 --server-mode=debug_servers --parsers-module-path=$parsers_path
    ```
    
    https://github.com/lastmile-ai/aiconfig/assets/151060367/123319cf-2e92-49e8-8f93-2d930a3b0ac7

--- python/src/aiconfig/editor/client/src/components/AIConfigEditor.tsx
+++ python/src/aiconfig/editor/client/src/components/AIConfigEditor.tsx
@@ -56,6 +56,7 @@ import {
 import { IconDeviceFloppy } from "@tabler/icons-react";
 import CopyButton from "./CopyButton";
 import AIConfigEditorThemeProvider from "../themes/AIConfigEditorThemeProvider";
+import DownloadButton from "./global/DownloadButton";
 import ShareButton from "./global/ShareButton";
 import PromptsContainer from "./prompt/PromptsContainer";
 
@@ -103,6 +104,7 @@ export type AIConfigCallbacks = {
   ) => Promise<{ aiconfig: AIConfig }>;
   clearOutputs: () => Promise<{ aiconfig: AIConfig }>;
   deletePrompt: (promptName: string) => Promise<void>;
+  download?: (config: AIConfig) => Promise<void>;
   getModels: (search: string) => Promise<string[]>;
   getServerStatus?: () => Promise<{ status: "OK" | "ERROR" }>;
   logEventHandler?: (event: LogEvent, data?: LogEventData) => void;
@@ -149,6 +151,26 @@ export default function AIConfigEditor({
   stateRef.current = aiconfigState;
 
   const logEventHandler = callbacks?.logEventHandler;
+
+  const downloadCallback = callbacks?.download;
+  const onDownload = useCallback(
+    async (config: AIConfig) => {
+      if (!downloadCallback) {
+        return;
+      }
+      try {
+        await downloadCallback(config);
+      } catch (err: unknown) {
+        const message = (err as RequestCallbackError).message ?? null;
+        showNotification({
+          title: "Error downloading AIConfig",
+          message,
+          color: "red",
+        });
+      }
+    },
+    [downloadCallback]
+  );
 
   const shareCallback = callbacks?.share;
   const onShare = useCallback(async () => {
@@ -962,6 +984,9 @@ export default function AIConfigEditor({
             <Flex justify="flex-end" mt="md" mb="xs">
               {!readOnly && (
                 <Group>
+                  {downloadCallback && (
+                    <DownloadButton onDownload={onDownload} />
+                  )}
                   {shareCallback && <ShareButton onShare={onShare} />}
                   {onClearOutputs && (
                     <Button
