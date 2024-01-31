c41e49381835bb00e9eded1e8d1adfc1074e09fc
Ryan Holinshead <>
2024-01-31 15:51:13 -0500

    Support Markdown Rendering in Config Description
    # Support Markdown Rendering in Config Description
    
    One thing that would be nice for making configs more readable (especially in Gradio instructions -- https://huggingface.co/spaces/lastmileai/gradio-workbook-template) is if the description renderer supported markdown, similar to our output renderers. This PR swaps the non-editing view of the description to use our TextRenderer (which handles markdown, code, etc. rendering)
    
    ![Screenshot 2024-01-31 at 3 49 26 PM](https://github.com/lastmile-ai/aiconfig/assets/5060851/253307db-b05b-44b0-91fd-fd589e19af9d)
    ![Screenshot 2024-01-31 at 3 49 38 PM](https://github.com/lastmile-ai/aiconfig/assets/5060851/bc643f63-ea50-4ae1-8599-7d90fd4729cd)
    ![Screenshot 2024-01-31 at 3 50 09 PM](https://github.com/lastmile-ai/aiconfig/assets/5060851/bf84375e-d111-422d-8662-25f6ba6094e3)

--- python/src/aiconfig/editor/client/src/components/ConfigNameDescription.tsx
+++ python/src/aiconfig/editor/client/src/components/ConfigNameDescription.tsx
@@ -1,14 +1,8 @@
-import {
-  createStyles,
-  Stack,
-  Text,
-  Textarea,
-  TextInput,
-  Title,
-} from "@mantine/core";
+import { createStyles, Stack, Textarea, TextInput, Title } from "@mantine/core";
 import { useClickOutside } from "@mantine/hooks";
 import { memo, useCallback, useContext, useRef, useState } from "react";
 import AIConfigContext from "../contexts/AIConfigContext";
+import { TextRenderer } from "./prompt/TextRenderer";
 
 type Props = {
   name?: string;
@@ -42,6 +36,11 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+function isClickableChildElement(element: EventTarget | null) {
+  // Specifically handle markdown links and prism code copy svg elements
+  return element instanceof HTMLAnchorElement || element instanceof SVGElement;
+}
+
 export default memo(function ConfigNameDescription({
   name,
   description,
@@ -67,7 +66,8 @@ export default memo(function ConfigNameDescription({
 
   const onHandleEnter = useCallback(
     (event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      if (event.key === "Enter") {
+      if (event.key === "Enter" && !event.shiftKey) {
+        // Shift+Enter to add new line
         event.stopPropagation();
         setIsEditing(false);
       }
@@ -80,11 +80,16 @@ export default memo(function ConfigNameDescription({
       if (readOnly) {
         return;
       }
-      setIsEditing(true);
-      if (event.target === nameDisplayRef.current) {
+
+      if (event.currentTarget === nameDisplayRef.current) {
         setInitialFocus("name");
-      } else if (event.target === descriptionDisplayRef.current) {
+        setIsEditing(true);
+      } else if (
+        event.currentTarget === descriptionDisplayRef.current &&
+        !isClickableChildElement(event.target)
+      ) {
         setInitialFocus("description");
+        setIsEditing(true);
       }
     },
     [readOnly]
@@ -126,14 +131,15 @@ export default memo(function ConfigNameDescription({
           >
             {name}
           </Title>
-          <Text
-            ref={descriptionDisplayRef}
-            onClick={onClickEdit}
-            style={{ whiteSpace: "pre-wrap" }}
-            className={!readOnly ? classes.hoverContainer : undefined}
-          >
-            {description}
-          </Text>
+          {description && (
+            <div
+              ref={descriptionDisplayRef}
+              onClick={onClickEdit}
+              className={!readOnly ? classes.hoverContainer : undefined}
+            >
+              <TextRenderer content={description} />
+            </div>
+          )}
         </div>
       )}
     </Stack>
