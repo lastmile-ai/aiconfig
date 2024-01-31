4ad3ac2c86fd402da452b0c9c8a10d0e83e1d610
Rossdan Craig rossdan@lastmileai.dev <>
2024-01-31 10:14:40 -0500

    Create Claude and Azure cookbook demos
    Separate ones, also have env vars for Azure

---
+++ cookbooks/Claude-And-Azure/aiconfig_model_registry.py
@@ -0,0 +1,19 @@
+"""
+    Use this file to register model parsers that don't ship with aiconfig.
+    - Make sure your package is installed in the same environment as aiconfig.
+    - You must define a function `register_model_parsers() -> None` in this file.
+    - You should call `AIConfigRuntime.register_model_parser` in that function.
+
+    See example below.
+"""
+
+
+from aiconfig import AzureOpenAIParser, ModelParserRegistry
+import dotenv
+
+
+def register_model_parsers() -> None:
+    azure_gpt_35 = AzureOpenAIParser(deployment="gpt-35-turbo")
+    ModelParserRegistry.register_model_parser(azure_gpt_35)
+    
+    dotenv.load_dotenv()
