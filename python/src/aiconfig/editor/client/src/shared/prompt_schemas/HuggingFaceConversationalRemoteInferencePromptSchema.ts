de07d1bb3da60e026f5dfa37c7916c8457a4c04f
Ankush Pala  ankush@lastmileai.dev <>
2024-02-13 18:13:13 -0500

    [editor] Update HF conversationalprompt schema with parameters
    Taken from https://huggingface.co/docs/api-inference/detailed_parameters#conversational-task
    
    
    Skipped the options dictionary for now, they were not very relevant to inference.
    
    ## testplan
    
    https://github.com/huggingface/huggingface_hub/assets/141073967/59039705-64cd-453f-a09c-090c7beef597

--- python/src/aiconfig/editor/client/src/shared/prompt_schemas/HuggingFaceConversationalRemoteInferencePromptSchema.ts
+++ python/src/aiconfig/editor/client/src/shared/prompt_schemas/HuggingFaceConversationalRemoteInferencePromptSchema.ts
@@ -18,6 +18,46 @@ export const HuggingFaceConversationalRemoteInferencePromptSchema: PromptSchema 
         to a deployed Inference Endpoint`,
         default: "facebook/blenderbot-400M-distill",
       },
+      parameters: {
+        type: "object",
+        description: "Additional parameters for the conversational task.",
+        properties: {
+          min_length: {
+            type: "integer",
+            description: "Integer to define the minimum length in tokens of the output summary..",
+          },
+          max_length: {
+            type: "integer",
+            description: "Integer to define the maximum length in tokens of the output summary."
+          },
+          top_k: {
+            type: "integer",
+            description: "Integer to define the top tokens considered within the sample operation to create new text."
+          },
+          top_p: {
+            type: "number",
+            description: "Float to define the tokens that are within the sample operation of text generation. Add tokens in the sample for more probable to least probable until the sum of the probabilities is greater than top_p."
+          },
+          temperature: {
+            type: "number",
+            description: "The temperature of the sampling operation. 1 means regular sampling, 0 means always take the highest score, 100.0 is getting closer to uniform probability.",
+            minimum: 0,
+            maximum: 100,
+          },
+          repetition_penalty: {
+            type: "number",
+            description: "The more a token is used within generation the more it is penalized to not be picked in successive generation passes.",
+            minimum: 0,
+            maximum: 100
+          },
+          max_time: {
+            type: "number",
+            description: "The amount of time in seconds that the query should take maximum. Network can cause some overhead so it will be a soft limit.",
+            minimum: 0,
+            maximum: 120,
+          }
+        }
+      }
     },
   },
 };
