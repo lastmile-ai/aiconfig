efd4e7903d90e991b1a9b465309428b44557c3ec
Ankush Pala  ankush@lastmileai.dev <>
2024-02-21 16:10:28 -0500

    [vscode installation] 1/n Python version validation
    Add a helper method to validate the python interpreter version. Simply throws an error on a bad version. Notification dialog in diff on top.
    
    Align's with Python AIConfig's version requirement of python3.10 and higher
    
    ## Testplan
    
    Select a python interpreter 3.9, which is less than aiconfig's requirement version of 3.10.
    
    https://github.com/lastmile-ai/aiconfig/assets/141073967/4cfcb153-50aa-40fb-9764-54e964d4b1b8

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -18,6 +18,7 @@ import {
   getPythonPath,
   isSupportedConfigExtension,
   SUPPORTED_FILE_EXTENSIONS,
+  isPythonVersionAtLeast310
 } from "./util";
 import { AIConfigEditorProvider } from "./aiConfigEditor";
 import { AIConfigEditorManager } from "./aiConfigEditorManager";
@@ -673,7 +674,13 @@ async function checkPython() {
             }
           });
         resolve(false);
-      } else {
+      }
+      else if(!isPythonVersionAtLeast310(pythonPath)){
+        console.error("Python version is not 3.10 or higher. Please upgrade to Python 3.10 or higher.");
+        resolve(false);
+        // show guide for installation
+      } 
+      else if(stdout) {
         resolve(true);
         console.log("Python is installed");
       }
