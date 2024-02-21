465d1855fa43f7dac0fb20383d062b84957179d2
Ankush Pala  ankush@lastmileai.dev <>
2024-02-21 16:39:13 -0500

    [vscode installation] 2/n refactor error message for invalid python install
    Added a helper method for showing user friendly error when python install doesn't go well.
    
    ## Testplan
    
    Select Interpreter version 3.9.4. Open an editor and expect an error notification.
    
    https://github.com/lastmile-ai/aiconfig/assets/141073967/5bda86e0-690f-4e43-9e94-f9279e3b2f34

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -18,7 +18,8 @@ import {
   getPythonPath,
   isSupportedConfigExtension,
   SUPPORTED_FILE_EXTENSIONS,
-  isPythonVersionAtLeast310
+  isPythonVersionAtLeast310,
+  showGuideForInstallation,
 } from "./util";
 import { AIConfigEditorProvider } from "./aiConfigEditor";
 import { AIConfigEditorManager } from "./aiConfigEditorManager";
@@ -657,28 +658,16 @@ async function checkPython() {
       if (error) {
         console.error("Python was not found, can't install requirements");
         console.error("retrieved python path: " + pythonPath);
+        console.error("error: " + error);
 
         // Guide for installation
-        vscode.window
-          .showErrorMessage(
-            "Python is not installed",
-            ...["Install Python", "Retry"]
-          )
-          .then((selection) => {
-            if (selection === "Install Python") {
-              vscode.env.openExternal(
-                vscode.Uri.parse("https://www.python.org/downloads/")
-              );
-            } else if (selection === "Retry") {
-              vscode.commands.executeCommand(COMMANDS.INIT);
-            }
-          });
+        showGuideForInstallation("Specified Python Interpreter is not valid");
         resolve(false);
       }
       else if(!isPythonVersionAtLeast310(pythonPath)){
+        showGuideForInstallation("Python version is not 3.10 or higher. Please upgrade to Python 3.10 or higher.");
         console.error("Python version is not 3.10 or higher. Please upgrade to Python 3.10 or higher.");
         resolve(false);
-        // show guide for installation
       } 
       else if(stdout) {
         resolve(true);
