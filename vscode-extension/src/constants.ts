f2abf10545d2f998f6dd8dcd45a1930964ee3ab3
Ankush Pala  ankush@lastmileai.dev <>
2024-03-04 11:31:29 -0500

    [vscode] 7/n listeners and .env
    Summary:
    
    Test Plan:

--- vscode-extension/src/constants.ts
+++ vscode-extension/src/constants.ts
@@ -3,3 +3,5 @@ export const PYTHON_INTERPRETER_CACHE_KEY_NAME = "pythonInterpreter";
 // Used for prompting user to reload VS Code window on update, and ensuring
 // that walkthrough gets shown on first install
 export const VERSION_KEY_NAME = "version";
+
+export const ENV_FILE_PATH = "env_file_path";
--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -27,6 +27,7 @@ import {
   setupEnvironmentVariables,
   getConfigurationTarget,
   getModeFromDocument,
+  updateServerEnv
 } from "./util";
 import {
   initialize,
@@ -208,6 +209,25 @@ export async function activate(context: vscode.ExtensionContext) {
       }
     )
   );
+
+  // Handle changes to the .env path
+  context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(async (event) => {
+    if (event.affectsConfiguration("vscode-aiconfig")) {
+      // Get new env
+      const envPath = vscode.workspace.getConfiguration("vscode-aiconfig").get('envPath') as string;
+      console.log(`New .env path set: ${envPath}`);
+      // set env on all servesrs
+      const editors: Array<AIConfigEditorState> = Array.from(
+        aiconfigEditorManager.getRegisteredEditors()
+      );
+      if (editors.length > 0) {
+        editors.forEach(async (editor) => {
+          updateServerEnv(editor.editorServer.url, envPath);
+        });
+      
+      }
+  }
+  }));
 }
 
 // This method is called when your extension is deactivated
