cddf72bba2bd011355d88f98bdd1d7385de53883
Rossdan Craig rossdan@lastmileai.dev <>
2024-03-05 17:48:15 -0500

    [groq] Initialize extension for Groq OpenAI client wrapper
    Documentation here: https://console.groq.com/docs/quickstart
    
    Will publish extension after it's approved
    
    ## Test Plan
    
    https://github.com/lastmile-ai/aiconfig/assets/151060367/29437586-97de-427c-8480-2d9535304d05

---
+++ extensions/Groq/src/aiconfig_extension_groq/groq.py
@@ -0,0 +1,24 @@
+from groq import Groq
+from aiconfig.default_parsers.openai import DefaultOpenAIParser
+
+import os
+
+class GroqOpenAIParser(DefaultOpenAIParser):
+    def __init__(self, deployment: str):
+        super().__init__(model_id = deployment)
+
+        # Deployment is a custom name for the specific model they want to use
+        # when registering the Groq model parser. See the cookbook for reference
+        self.deployment = deployment
+
+    async def deserialize(self, *args, **kwargs):
+        # Logic doesn't depend on input, pass it forward to the openai model parser deserialize
+        openai_deserialized_params = await super().deserialize(*args, **kwargs)
+        openai_deserialized_params["model"] = self.deployment
+        return openai_deserialized_params
+
+    def initialize_openai_client(self) -> None:
+        # Initialize Groq Client
+        self.client = Groq(
+          api_key=os.getenv("GROQ_API_KEY"),
+        )
