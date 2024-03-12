b130132085fdc9ce1f9a27791e6f467f00cc51ee
Ryan Holinshead <>
2024-03-12 10:44:18 -0400

    [1/n] Move Global Parameters into ConfigMetadataContainer

--- python/src/aiconfig/editor/client/src/components/global/ConfigMetadataContainer.tsx
+++ python/src/aiconfig/editor/client/src/components/global/ConfigMetadataContainer.tsx
@@ -1,43 +1,44 @@
 import { Accordion, Text, createStyles } from "@mantine/core";
 import { JSONObject } from "aiconfig";
 import { memo, useContext, useState } from "react";
-import ParametersRenderer from "./ParametersRenderer";
-import AIConfigContext from "../contexts/AIConfigContext";
-import { PROMPT_CELL_LEFT_MARGIN_PX } from "../utils/constants";
+import AIConfigContext from "../../contexts/AIConfigContext";
+import { PROMPT_CELL_LEFT_MARGIN_PX } from "../../utils/constants";
+import ParametersRenderer from "../ParametersRenderer";
 
 type Props = {
-  initialValue: JSONObject;
+  metadata: JSONObject;
   onUpdateParameters: (newParameters: JSONObject) => void;
 };
 
 const useStyles = createStyles(() => ({
-  parametersContainer: {
+  configMetadataContainer: {
     margin: `16px auto 16px ${PROMPT_CELL_LEFT_MARGIN_PX}px`,
   },
-  parametersContainerReadonly: {
+  configMetadataContainerReadonly: {
     margin: "16px auto",
   },
 }));
 
-export default memo(function GlobalParametersContainer({
-  initialValue,
+export default memo(function ConfigMetadataContainer({
+  metadata,
   onUpdateParameters,
 }: Props) {
-  const [isParametersDrawerOpen, setIsParametersDrawerOpen] = useState(false);
-
   const { classes } = useStyles();
   const { readOnly } = useContext(AIConfigContext);
+  const [openPanel, setOpenPanel] = useState<string | null>(null);
 
   return (
     // Set local and global classname. Global will override if specified
-    // Local is readonly or not. Global will always have parametersContainer class
-    // and if readonly, will also have parametersContainerReadonly class (to allow overrides)
+    // Local is readonly or not. Global will always have configMetadataContainer class
+    // and if readonly, will also have configMetadataContainerReadonly class (to allow overrides)
     <div
       className={`${
         readOnly
-          ? classes.parametersContainerReadonly
-          : classes.parametersContainer
-      } parametersContainer ${readOnly ? "parametersContainerReadonly" : ""}`}
+          ? classes.configMetadataContainerReadonly
+          : classes.configMetadataContainer
+      } configMetadataContainer ${
+        readOnly ? "configMetadataContainerReadonly" : ""
+      }`}
     >
       <Accordion
         styles={{
@@ -49,16 +50,16 @@ export default memo(function GlobalParametersContainer({
             fontSize: "0.85em",
           },
         }}
-        onChange={(value) => setIsParametersDrawerOpen(value === "parameters")}
+        onChange={(value) => setOpenPanel(value)}
       >
         <Accordion.Item value="parameters">
           <Accordion.Control>
             <Text color="blue">Global Parameters {"{}"}</Text>
           </Accordion.Control>
           <Accordion.Panel>
-            {isParametersDrawerOpen && (
+            {openPanel === "parameters" && (
               <ParametersRenderer
-                initialValue={initialValue}
+                initialValue={metadata?.parameters ?? {}}
                 onUpdateParameters={onUpdateParameters}
                 maxHeight="300px"
               />
